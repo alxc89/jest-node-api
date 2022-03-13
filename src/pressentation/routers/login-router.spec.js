4ef8fc3ee1268c01f4e8aafe5722163fff0801fe
alxc89 <alexcaardoso@gmail.com>
2022-03-13 00:35:43 -0300

    verifica se o loginRouter retorna 400 quando não é passado senha

--- src/pressentation/routers/login-router.spec.js
+++ src/pressentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -8,12 +8,25 @@ class LoginRouter {
   }
 }
 
-describe('', () => {
+describe('Login Router', () => {
   test('Should return 400 if no email is provided', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
         password: 'any_password'
+      }
+    }
+    const httpReponse = sut.route(httpRequest)
+    expect(httpReponse.statusCode).toBe(400)
+  })
+})
+
+describe('Login Router', () => {
+  test('Should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@teste.com'
       }
     }
     const httpReponse = sut.route(httpRequest)
