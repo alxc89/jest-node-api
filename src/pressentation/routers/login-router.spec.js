6cea348688de609ea393aaf56904159768f8e390
alxc89 <alexcaardoso@gmail.com>
2022-03-13 00:45:20 -0300

    verifica se o loginRouter retorna 500 quando não é passado httpRequest e body

--- src/pressentation/routers/login-router.spec.js
+++ src/pressentation/routers/login-router.spec.js
@@ -1,5 +1,10 @@
 class LoginRouter {
   route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
     const { email, password } = httpRequest.body
     if (!email || !password) {
       return {
@@ -32,14 +37,16 @@ describe('Login Router', () => {
     expect(httpReponse.statusCode).toBe(400)
   })
 
-  test('Should return 400 if no password is provided', () => {
+  test('Should return 500 if httpRequest is provided', () => {
     const sut = new LoginRouter()
-    const httpRequest = {
-      body: {
-        email: 'any_email@teste.com'
-      }
-    }
+    const httpReponse = sut.route()
+    expect(httpReponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if httpRequest has no body', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {}
     const httpReponse = sut.route(httpRequest)
-    expect(httpReponse.statusCode).toBe(400)
+    expect(httpReponse.statusCode).toBe(500)
   })
 })
