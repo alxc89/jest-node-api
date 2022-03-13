6ad676033f9a9918ba910cd9b3789abd4d87fa4d
alxc89 <alexcaardoso@gmail.com>
2022-03-13 20:39:07 -0300

    movendo erro 401 para o metodo helper

--- src/presentation/helpers/Http-response.js
+++ src/presentation/helpers/Http-response.js
@@ -1,4 +1,5 @@
 const MissingParamError = require('./Missing-param-error')
+const UnauthorizedError = require('./Unauthorized-error')
 
 class HttpReponse {
   static badRequest (paramName) {
@@ -11,6 +12,13 @@ class HttpReponse {
   static serverError () {
     return {
       statusCode: 500
+    }
+  }
+
+  static unauthorizedError () {
+    return {
+      statusCode: 401,
+      body: new UnauthorizedError()
     }
   }
 }
