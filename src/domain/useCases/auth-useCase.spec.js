d558e118379ca758f3720e267c3e14f958323fd2
alxc89 <alexcaardoso@gmail.com>
2022-03-20 15:44:11 -0300

    verificando se AuthUseCase retorna erro se náo passar o password

--- src/domain/useCases/auth-useCase.spec.js
+++ src/domain/useCases/auth-useCase.spec.js
@@ -1,9 +1,12 @@
 const { MissingParamError } = require('../../utils/errors')
 
 class AuthUseCase {
-  async auth (email) {
+  async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
+    }
+    if (!password) {
+      throw new MissingParamError('password')
     }
   }
 }
@@ -13,5 +16,11 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth() // retirnado o await pq o jest não suporta. Então vai receber uma promise
     expect(promise).rejects.toThrow(new MissingParamError('email'))// pegando o rejects da promise e verificando se é uma execeção
+  })
+
+  test('Should throw if no password is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@gmail.com') // retirnado o await pq o jest não suporta. Então vai receber uma promise
+    expect(promise).rejects.toThrow(new MissingParamError('password'))// pegando o rejects da promise e verificando se é uma execeção
   })
 })
